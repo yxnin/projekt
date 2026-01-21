faec2187725f0776fb134353dfc89d92d5dd2af5
Michal Postawa <m.postawa13@gmail.com>
2026-01-21 16:15:30 +0100

    Remove ConsoleApp project

--- DentalClinic.ConsoleApp/AppSession.cs
+++
@@ -1,8 +0,0 @@
-﻿using DentalClinic.Core.Entities;
-
-public class AppSession
-{
-    public User? CurrentUser { get; private set; }
-    public void SignIn(User user) => CurrentUser = user;
-    public void SignOut() => CurrentUser = null;
-}

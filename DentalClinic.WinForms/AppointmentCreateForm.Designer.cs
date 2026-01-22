ac9e7f816072647f3d65f89b466b473301f36e20
Michal Postawa <m.postawa13@gmail.com>
2026-01-22 17:16:15 +0100

    17:16 22/01

--- DentalClinic.WinForms/AppointmentCreateForm.Designer.cs
+++ DentalClinic.WinForms/AppointmentCreateForm.Designer.cs
@@ -26,116 +26,187 @@
 
         private void InitializeComponent()
         {
-            this.table = new System.Windows.Forms.TableLayoutPanel();
-            this.lblPatient = new System.Windows.Forms.Label();
-            this.lblDentist = new System.Windows.Forms.Label();
-            this.lblService = new System.Windows.Forms.Label();
-            this.lblStart = new System.Windows.Forms.Label();
-            this.cbPatient = new System.Windows.Forms.ComboBox();
-            this.cbDentist = new System.Windows.Forms.ComboBox();
-            this.cbService = new System.Windows.Forms.ComboBox();
-            this.dtStart = new System.Windows.Forms.DateTimePicker();
-            this.panelButtons = new System.Windows.Forms.FlowLayoutPanel();
-            this.btnOk = new System.Windows.Forms.Button();
-            this.btnCancel = new System.Windows.Forms.Button();
-            this.table.SuspendLayout();
-            this.panelButtons.SuspendLayout();
-            this.SuspendLayout();
+            table = new TableLayoutPanel();
+            lblPatient = new Label();
+            cbPatient = new ComboBox();
+            lblDentist = new Label();
+            cbDentist = new ComboBox();
+            lblService = new Label();
+            cbService = new ComboBox();
+            lblStart = new Label();
+            dtStart = new DateTimePicker();
+            panelButtons = new FlowLayoutPanel();
+            btnOk = new Button();
+            btnCancel = new Button();
+            table.SuspendLayout();
+            panelButtons.SuspendLayout();
+            SuspendLayout();
             // 
             // table
             // 
-            this.table.ColumnCount = 2;
-            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
-            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.table.Controls.Add(this.lblPatient, 0, 0);
-            this.table.Controls.Add(this.cbPatient, 1, 0);
-            this.table.Controls.Add(this.lblDentist, 0, 1);
-            this.table.Controls.Add(this.cbDentist, 1, 1);
-            this.table.Controls.Add(this.lblService, 0, 2);
-            this.table.Controls.Add(this.cbService, 1, 2);
-            this.table.Controls.Add(this.lblStart, 0, 3);
-            this.table.Controls.Add(this.dtStart, 1, 3);
-            this.table.Dock = System.Windows.Forms.DockStyle.Top;
-            this.table.Location = new System.Drawing.Point(12, 12);
-            this.table.Name = "table";
-            this.table.RowCount = 4;
-            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 34F));
-            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 34F));
-            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 34F));
-            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 34F));
-            this.table.Size = new System.Drawing.Size(560, 136);
-            this.table.TabIndex = 0;
+            table.ColumnCount = 2;
+            table.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 105F));
+            table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            table.Controls.Add(lblPatient, 0, 0);
+            table.Controls.Add(cbPatient, 1, 0);
+            table.Controls.Add(lblDentist, 0, 1);
+            table.Controls.Add(cbDentist, 1, 1);
+            table.Controls.Add(lblService, 0, 2);
+            table.Controls.Add(cbService, 1, 2);
+            table.Controls.Add(lblStart, 0, 3);
+            table.Controls.Add(dtStart, 1, 3);
+            table.Dock = DockStyle.Top;
+            table.Location = new Point(10, 9);
+            table.Margin = new Padding(3, 2, 3, 2);
+            table.Name = "table";
+            table.RowCount = 4;
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, 26F));
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, 26F));
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, 26F));
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, 26F));
+            table.Size = new Size(491, 102);
+            table.TabIndex = 0;
             // 
-            // labels
+            // lblPatient
             // 
-            this.lblPatient.AutoSize = true; this.lblPatient.Dock = System.Windows.Forms.DockStyle.Fill; this.lblPatient.Text = "Patient"; this.lblPatient.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.lblDentist.AutoSize = true; this.lblDentist.Dock = System.Windows.Forms.DockStyle.Fill; this.lblDentist.Text = "Dentist"; this.lblDentist.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.lblService.AutoSize = true; this.lblService.Dock = System.Windows.Forms.DockStyle.Fill; this.lblService.Text = "Service"; this.lblService.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.lblStart.AutoSize = true; this.lblStart.Dock = System.Windows.Forms.DockStyle.Fill; this.lblStart.Text = "Start"; this.lblStart.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            lblPatient.AutoSize = true;
+            lblPatient.Dock = DockStyle.Fill;
+            lblPatient.Location = new Point(3, 0);
+            lblPatient.Name = "lblPatient";
+            lblPatient.Size = new Size(99, 26);
+            lblPatient.TabIndex = 0;
+            lblPatient.Text = "Patient";
+            lblPatient.TextAlign = ContentAlignment.MiddleLeft;
             // 
-            // comboboxes
+            // cbPatient
             // 
-            this.cbPatient.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList; this.cbPatient.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.cbDentist.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList; this.cbDentist.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.cbService.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList; this.cbService.Dock = System.Windows.Forms.DockStyle.Fill;
+            cbPatient.Dock = DockStyle.Fill;
+            cbPatient.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbPatient.Location = new Point(108, 2);
+            cbPatient.Margin = new Padding(3, 2, 3, 2);
+            cbPatient.Name = "cbPatient";
+            cbPatient.Size = new Size(380, 23);
+            cbPatient.TabIndex = 1;
+            // 
+            // lblDentist
+            // 
+            lblDentist.AutoSize = true;
+            lblDentist.Dock = DockStyle.Fill;
+            lblDentist.Location = new Point(3, 26);
+            lblDentist.Name = "lblDentist";
+            lblDentist.Size = new Size(99, 26);
+            lblDentist.TabIndex = 2;
+            lblDentist.Text = "Dentist";
+            lblDentist.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // cbDentist
+            // 
+            cbDentist.Dock = DockStyle.Fill;
+            cbDentist.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbDentist.Location = new Point(108, 28);
+            cbDentist.Margin = new Padding(3, 2, 3, 2);
+            cbDentist.Name = "cbDentist";
+            cbDentist.Size = new Size(380, 23);
+            cbDentist.TabIndex = 3;
+            // 
+            // lblService
+            // 
+            lblService.AutoSize = true;
+            lblService.Dock = DockStyle.Fill;
+            lblService.Location = new Point(3, 52);
+            lblService.Name = "lblService";
+            lblService.Size = new Size(99, 26);
+            lblService.TabIndex = 4;
+            lblService.Text = "Service";
+            lblService.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // cbService
+            // 
+            cbService.Dock = DockStyle.Fill;
+            cbService.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbService.Location = new Point(108, 54);
+            cbService.Margin = new Padding(3, 2, 3, 2);
+            cbService.Name = "cbService";
+            cbService.Size = new Size(380, 23);
+            cbService.TabIndex = 5;
+            // 
+            // lblStart
+            // 
+            lblStart.AutoSize = true;
+            lblStart.Dock = DockStyle.Fill;
+            lblStart.Location = new Point(3, 78);
+            lblStart.Name = "lblStart";
+            lblStart.Size = new Size(99, 26);
+            lblStart.TabIndex = 6;
+            lblStart.Text = "Start";
+            lblStart.TextAlign = ContentAlignment.MiddleLeft;
             // 
             // dtStart
             // 
-            this.dtStart.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dtStart.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dtStart.CustomFormat = "yyyy-MM-dd HH:mm";
+            dtStart.CustomFormat = "yyyy-MM-dd HH:mm";
+            dtStart.Dock = DockStyle.Fill;
+            dtStart.Format = DateTimePickerFormat.Custom;
+            dtStart.Location = new Point(108, 80);
+            dtStart.Margin = new Padding(3, 2, 3, 2);
+            dtStart.Name = "dtStart";
+            dtStart.Size = new Size(380, 23);
+            dtStart.TabIndex = 7;
             // 
             // panelButtons
             // 
-            this.panelButtons.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panelButtons.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
-            this.panelButtons.Location = new System.Drawing.Point(12, 160);
-            this.panelButtons.Name = "panelButtons";
-            this.panelButtons.Size = new System.Drawing.Size(560, 40);
-            this.panelButtons.TabIndex = 1;
+            panelButtons.Controls.Add(btnOk);
+            panelButtons.Controls.Add(btnCancel);
+            panelButtons.Dock = DockStyle.Bottom;
+            panelButtons.FlowDirection = FlowDirection.RightToLeft;
+            panelButtons.Location = new Point(10, 120);
+            panelButtons.Margin = new Padding(3, 2, 3, 2);
+            panelButtons.Name = "panelButtons";
+            panelButtons.Size = new Size(491, 30);
+            panelButtons.TabIndex = 1;
             // 
             // btnOk
             // 
-            this.btnOk.Location = new System.Drawing.Point(482, 3);
-            this.btnOk.Name = "btnOk";
-            this.btnOk.Size = new System.Drawing.Size(75, 29);
-            this.btnOk.TabIndex = 0;
-            this.btnOk.Text = "OK";
-            this.btnOk.UseVisualStyleBackColor = true;
-            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            btnOk.Location = new Point(422, 2);
+            btnOk.Margin = new Padding(3, 2, 3, 2);
+            btnOk.Name = "btnOk";
+            btnOk.Size = new Size(66, 22);
+            btnOk.TabIndex = 0;
+            btnOk.Text = "OK";
+            btnOk.UseVisualStyleBackColor = true;
+            btnOk.Click += btnOk_Click;
             // 
             // btnCancel
             // 
-            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(401, 3);
-            this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(75, 29);
-            this.btnCancel.TabIndex = 1;
-            this.btnCancel.Text = "Cancel";
-            this.btnCancel.UseVisualStyleBackColor = true;
+            btnCancel.DialogResult = DialogResult.Cancel;
+            btnCancel.Location = new Point(350, 2);
+            btnCancel.Margin = new Padding(3, 2, 3, 2);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(66, 22);
+            btnCancel.TabIndex = 1;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = true;
             // 
             // AppointmentCreateForm
             // 
-            this.AcceptButton = this.btnOk;
-            this.CancelButton = this.btnCancel;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 212);
-            this.Controls.Add(this.panelButtons);
-            this.Controls.Add(this.table);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "AppointmentCreateForm";
-            this.Padding = new System.Windows.Forms.Padding(12);
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Add appointment";
-            this.panelButtons.Controls.Add(this.btnOk);
-            this.panelButtons.Controls.Add(this.btnCancel);
-            this.table.ResumeLayout(false);
-            this.table.PerformLayout();
-            this.panelButtons.ResumeLayout(false);
-            this.ResumeLayout(false);
+            AcceptButton = btnOk;
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            CancelButton = btnCancel;
+            ClientSize = new Size(511, 159);
+            Controls.Add(panelButtons);
+            Controls.Add(table);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Margin = new Padding(3, 2, 3, 2);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "AppointmentCreateForm";
+            Padding = new Padding(10, 9, 10, 9);
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "Add appointment";
+            table.ResumeLayout(false);
+            table.PerformLayout();
+            panelButtons.ResumeLayout(false);
+            ResumeLayout(false);
         }
     }
 }
--- DentalClinic.WinForms/DentistEditForm.Designer.cs
+++ DentalClinic.WinForms/DentistEditForm.Designer.cs
@@ -28,111 +28,182 @@
 
         private void InitializeComponent()
         {
-            this.table = new System.Windows.Forms.TableLayoutPanel();
-            this.lblFirst = new System.Windows.Forms.Label();
-            this.lblLast = new System.Windows.Forms.Label();
-            this.lblPhone = new System.Windows.Forms.Label();
-            this.lblSpec = new System.Windows.Forms.Label();
-            this.tbFirstName = new System.Windows.Forms.TextBox();
-            this.tbLastName = new System.Windows.Forms.TextBox();
-            this.tbPhone = new System.Windows.Forms.TextBox();
-            this.tbSpecialization = new System.Windows.Forms.TextBox();
-            this.panelButtons = new System.Windows.Forms.FlowLayoutPanel();
-            this.btnOk = new System.Windows.Forms.Button();
-            this.btnCancel = new System.Windows.Forms.Button();
-            this.table.SuspendLayout();
-            this.panelButtons.SuspendLayout();
-            this.SuspendLayout();
+            table = new TableLayoutPanel();
+            lblFirst = new Label();
+            tbFirstName = new TextBox();
+            lblLast = new Label();
+            tbLastName = new TextBox();
+            lblPhone = new Label();
+            tbPhone = new TextBox();
+            lblSpec = new Label();
+            tbSpecialization = new TextBox();
+            panelButtons = new FlowLayoutPanel();
+            btnOk = new Button();
+            btnCancel = new Button();
+            table.SuspendLayout();
+            panelButtons.SuspendLayout();
+            SuspendLayout();
             // 
             // table
             // 
-            this.table.ColumnCount = 2;
-            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
-            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.table.Controls.Add(this.lblFirst, 0, 0);
-            this.table.Controls.Add(this.tbFirstName, 1, 0);
-            this.table.Controls.Add(this.lblLast, 0, 1);
-            this.table.Controls.Add(this.tbLastName, 1, 1);
-            this.table.Controls.Add(this.lblPhone, 0, 2);
-            this.table.Controls.Add(this.tbPhone, 1, 2);
-            this.table.Controls.Add(this.lblSpec, 0, 3);
-            this.table.Controls.Add(this.tbSpecialization, 1, 3);
-            this.table.Dock = System.Windows.Forms.DockStyle.Top;
-            this.table.Location = new System.Drawing.Point(12, 12);
-            this.table.Name = "table";
-            this.table.RowCount = 4;
-            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 34F));
-            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 34F));
-            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 34F));
-            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 34F));
-            this.table.Size = new System.Drawing.Size(460, 136);
-            this.table.TabIndex = 0;
+            table.ColumnCount = 2;
+            table.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 105F));
+            table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            table.Controls.Add(lblFirst, 0, 0);
+            table.Controls.Add(tbFirstName, 1, 0);
+            table.Controls.Add(lblLast, 0, 1);
+            table.Controls.Add(tbLastName, 1, 1);
+            table.Controls.Add(lblPhone, 0, 2);
+            table.Controls.Add(tbPhone, 1, 2);
+            table.Controls.Add(lblSpec, 0, 3);
+            table.Controls.Add(tbSpecialization, 1, 3);
+            table.Dock = DockStyle.Top;
+            table.Location = new Point(10, 9);
+            table.Margin = new Padding(3, 2, 3, 2);
+            table.Name = "table";
+            table.RowCount = 4;
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, 26F));
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, 26F));
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, 26F));
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, 26F));
+            table.Size = new Size(404, 102);
+            table.TabIndex = 0;
             // 
-            // labels
+            // lblFirst
             // 
-            this.lblFirst.AutoSize = true; this.lblFirst.Dock = System.Windows.Forms.DockStyle.Fill; this.lblFirst.Text = "First name"; this.lblFirst.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.lblLast.AutoSize = true; this.lblLast.Dock = System.Windows.Forms.DockStyle.Fill; this.lblLast.Text = "Last name"; this.lblLast.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.lblPhone.AutoSize = true; this.lblPhone.Dock = System.Windows.Forms.DockStyle.Fill; this.lblPhone.Text = "Phone"; this.lblPhone.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.lblSpec.AutoSize = true; this.lblSpec.Dock = System.Windows.Forms.DockStyle.Fill; this.lblSpec.Text = "Specialization"; this.lblSpec.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            lblFirst.AutoSize = true;
+            lblFirst.Dock = DockStyle.Fill;
+            lblFirst.Location = new Point(3, 0);
+            lblFirst.Name = "lblFirst";
+            lblFirst.Size = new Size(99, 26);
+            lblFirst.TabIndex = 0;
+            lblFirst.Text = "First name";
+            lblFirst.TextAlign = ContentAlignment.MiddleLeft;
             // 
-            // textboxes
+            // tbFirstName
             // 
-            this.tbFirstName.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tbLastName.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tbPhone.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tbSpecialization.Dock = System.Windows.Forms.DockStyle.Fill;
+            tbFirstName.Dock = DockStyle.Fill;
+            tbFirstName.Location = new Point(108, 2);
+            tbFirstName.Margin = new Padding(3, 2, 3, 2);
+            tbFirstName.Name = "tbFirstName";
+            tbFirstName.Size = new Size(293, 23);
+            tbFirstName.TabIndex = 1;
+            // 
+            // lblLast
+            // 
+            lblLast.AutoSize = true;
+            lblLast.Dock = DockStyle.Fill;
+            lblLast.Location = new Point(3, 26);
+            lblLast.Name = "lblLast";
+            lblLast.Size = new Size(99, 26);
+            lblLast.TabIndex = 2;
+            lblLast.Text = "Last name";
+            lblLast.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // tbLastName
+            // 
+            tbLastName.Dock = DockStyle.Fill;
+            tbLastName.Location = new Point(108, 28);
+            tbLastName.Margin = new Padding(3, 2, 3, 2);
+            tbLastName.Name = "tbLastName";
+            tbLastName.Size = new Size(293, 23);
+            tbLastName.TabIndex = 3;
+            // 
+            // lblPhone
+            // 
+            lblPhone.AutoSize = true;
+            lblPhone.Dock = DockStyle.Fill;
+            lblPhone.Location = new Point(3, 52);
+            lblPhone.Name = "lblPhone";
+            lblPhone.Size = new Size(99, 26);
+            lblPhone.TabIndex = 4;
+            lblPhone.Text = "Phone";
+            lblPhone.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // tbPhone
+            // 
+            tbPhone.Dock = DockStyle.Fill;
+            tbPhone.Location = new Point(108, 54);
+            tbPhone.Margin = new Padding(3, 2, 3, 2);
+            tbPhone.Name = "tbPhone";
+            tbPhone.Size = new Size(293, 23);
+            tbPhone.TabIndex = 5;
+            // 
+            // lblSpec
+            // 
+            lblSpec.AutoSize = true;
+            lblSpec.Dock = DockStyle.Fill;
+            lblSpec.Location = new Point(3, 78);
+            lblSpec.Name = "lblSpec";
+            lblSpec.Size = new Size(99, 26);
+            lblSpec.TabIndex = 6;
+            lblSpec.Text = "Specialization";
+            lblSpec.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // tbSpecialization
+            // 
+            tbSpecialization.Dock = DockStyle.Fill;
+            tbSpecialization.Location = new Point(108, 80);
+            tbSpecialization.Margin = new Padding(3, 2, 3, 2);
+            tbSpecialization.Name = "tbSpecialization";
+            tbSpecialization.Size = new Size(293, 23);
+            tbSpecialization.TabIndex = 7;
             // 
             // panelButtons
             // 
-            this.panelButtons.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panelButtons.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
-            this.panelButtons.Location = new System.Drawing.Point(12, 160);
-            this.panelButtons.Name = "panelButtons";
-            this.panelButtons.Size = new System.Drawing.Size(460, 40);
-            this.panelButtons.TabIndex = 1;
+            panelButtons.Controls.Add(btnOk);
+            panelButtons.Controls.Add(btnCancel);
+            panelButtons.Dock = DockStyle.Bottom;
+            panelButtons.FlowDirection = FlowDirection.RightToLeft;
+            panelButtons.Location = new Point(10, 120);
+            panelButtons.Margin = new Padding(3, 2, 3, 2);
+            panelButtons.Name = "panelButtons";
+            panelButtons.Size = new Size(404, 30);
+            panelButtons.TabIndex = 1;
             // 
             // btnOk
             // 
-            this.btnOk.Location = new System.Drawing.Point(382, 3);
-            this.btnOk.Name = "btnOk";
-            this.btnOk.Size = new System.Drawing.Size(75, 29);
-            this.btnOk.TabIndex = 0;
-            this.btnOk.Text = "OK";
-            this.btnOk.UseVisualStyleBackColor = true;
-            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            btnOk.Location = new Point(335, 2);
+            btnOk.Margin = new Padding(3, 2, 3, 2);
+            btnOk.Name = "btnOk";
+            btnOk.Size = new Size(66, 22);
+            btnOk.TabIndex = 0;
+            btnOk.Text = "OK";
+            btnOk.UseVisualStyleBackColor = true;
+            btnOk.Click += btnOk_Click;
             // 
             // btnCancel
             // 
-            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(301, 3);
-            this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(75, 29);
-            this.btnCancel.TabIndex = 1;
-            this.btnCancel.Text = "Cancel";
-            this.btnCancel.UseVisualStyleBackColor = true;
+            btnCancel.DialogResult = DialogResult.Cancel;
+            btnCancel.Location = new Point(263, 2);
+            btnCancel.Margin = new Padding(3, 2, 3, 2);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(66, 22);
+            btnCancel.TabIndex = 1;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = true;
             // 
             // DentistEditForm
             // 
-            this.AcceptButton = this.btnOk;
-            this.CancelButton = this.btnCancel;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(484, 212);
-            this.Controls.Add(this.panelButtons);
-            this.Controls.Add(this.table);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "DentistEditForm";
-            this.Padding = new System.Windows.Forms.Padding(12);
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Dentist";
-            this.panelButtons.Controls.Add(this.btnOk);
-            this.panelButtons.Controls.Add(this.btnCancel);
-            this.table.ResumeLayout(false);
-            this.table.PerformLayout();
-            this.panelButtons.ResumeLayout(false);
-            this.ResumeLayout(false);
+            AcceptButton = btnOk;
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            CancelButton = btnCancel;
+            ClientSize = new Size(424, 159);
+            Controls.Add(panelButtons);
+            Controls.Add(table);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Margin = new Padding(3, 2, 3, 2);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "DentistEditForm";
+            Padding = new Padding(10, 9, 10, 9);
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "Dentist";
+            table.ResumeLayout(false);
+            table.PerformLayout();
+            panelButtons.ResumeLayout(false);
+            ResumeLayout(false);
         }
     }
 }
